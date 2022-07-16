061ad0a7e51548fdbdd1ca2f2aac2503d6a4b9ab
orshalmayev <orsh@ofakim-group.com>
2022-07-16 19:15:20 +0300

    added action for adding a new message

--- ngrx-chat-app/src/app/core/state/thread/thread.actions.ts
+++ ngrx-chat-app/src/app/core/state/thread/thread.actions.ts
@@ -1,10 +1,26 @@
-import { IThread } from "@models/thread.model";
-import { createActionGroup, props } from "@ngrx/store";
-import { EStateFeatures } from "../app/app.types";
+import { IMessage } from '@models/message.model';
+import { IThread } from '@models/thread.model';
+import { createActionGroup, props } from '@ngrx/store';
+import { generateUUID } from '@shared/utils';
+import { EStateFeatures } from '../app/app.types';
 
 export const threadActions = createActionGroup({
     source: EStateFeatures.Threads,
     events: {
-        'Add': props<{thread: IThread}>(),
-    }
-})
+        'Add Thread': props<{ thread: IThread }>(),
+        'Add Message': (thread: IThread, message: IMessage) => {
+            const defaults = {
+                id: generateUUID(),
+                sentAt: new Date(),
+                isRead: false,
+                thread: thread,
+            };
+            const messageWithDefaults: IMessage = {...defaults, ...message};
+
+            return {
+                thread: thread,
+                message: messageWithDefaults,
+            };
+        },
+    },
+});
--- ngrx-chat-app/src/app/core/state/thread/thread.reducer.ts
+++ ngrx-chat-app/src/app/core/state/thread/thread.reducer.ts
@@ -1,6 +1,6 @@
-import { createReducer, on } from "@ngrx/store";
-import { threadActions } from "./thread.actions";
-import { IThreadsState } from "./thread.types";
+import { createReducer, on } from '@ngrx/store';
+import { threadActions } from './thread.actions';
+import { IThreadsState } from './thread.types';
 
 const initialState: IThreadsState = {
     ids: [],
@@ -10,19 +10,41 @@ const initialState: IThreadsState = {
 
 export const threadsReducer = createReducer(
     initialState,
-    // ADD
-    on(
-        threadActions.add,
-        (state, {thread}) => {
-            if (state.ids.includes(thread.id)) {return state};
-
-            return {
-                ids: [ ...state.ids, thread.id ],
-                currentThreadId: state.currentThreadId,
-                entities: Object.assign({}, state.entities, {
-                    [thread.id]: thread
-                })
-            };
+    // ADD THREAD
+    on(threadActions.addThread, (state, { thread }) => {
+        if (state.ids.includes(thread.id)) {
+            return state;
         }
-    ),
-);// END createReducer
+
+        return {
+            ids: [...state.ids, thread.id],
+            currentThreadId: state.currentThreadId,
+            entities: Object.assign({}, state.entities, {
+                [thread.id]: thread,
+            }),
+        };
+    }),
+    // ADD MESSAGE
+    on(threadActions.addMessage, (state, {thread, message}) => {
+        // special case: if the message being added is in the current thread, then
+        // mark it as read
+        const isRead = message?.thread?.id === state.currentThreadId ? true : message.isRead;
+        const newMessage = Object.assign({}, message, { isRead: isRead });
+
+        // grab the old thread from entities
+        const oldThread = state.entities[thread.id];
+
+        // create a new thread which has our newMessage
+        const newThread = Object.assign({}, oldThread, {
+            messages: [...oldThread.messages, newMessage],
+        });
+
+        return {
+            ids: state.ids, // unchanged
+            currentThreadId: state.currentThreadId, // unchanged
+            entities: Object.assign({}, state.entities, {
+                [thread.id]: newThread,
+            }),
+        };
+    }),
+); // END createReducer
